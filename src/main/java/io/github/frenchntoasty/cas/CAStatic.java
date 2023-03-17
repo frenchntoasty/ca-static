0cfd669ec718d847fd1249a63e8481751d6aff64
BuildTools <unconfigured@null.spigotmc.org>
2023-03-16 23:56:59 -0400

    remove weather related gamerule

--- src/main/java/io/github/frenchntoasty/cas/CAStatic.java
+++ src/main/java/io/github/frenchntoasty/cas/CAStatic.java
@@ -37,7 +37,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class CAStatic {
     public static final String MODID = "castatic";
     public static final String NAME = "CA-Static";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.2";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
@@ -52,7 +52,6 @@ public class CAStatic {
         WorldServer world = server.getWorld(0);
         GameRules rules = world.getGameRules();
         rules.setOrCreateGameRule("doFireTick", "false");
-		rules.setOrCreateGameRule("doWeatherCycle", "false");
 		rules.setOrCreateGameRule("commandBlockOutput", "false");
 		rules.setOrCreateGameRule("mobGriefing", "false");
     }
