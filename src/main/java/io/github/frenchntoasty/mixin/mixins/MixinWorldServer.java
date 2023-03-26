7d0cef1ecc481dd1af0d4ef5c294adfb35b96441
frenchntoasty <poilut5@hotmail.com>
2023-03-25 20:59:03 -0400

    mixin fix

--- src/main/java/io/github/frenchntoasty/mixin/mixins/MixinWorldServer.java
+++ src/main/java/io/github/frenchntoasty/mixin/mixins/MixinWorldServer.java
@@ -1,29 +1,20 @@
 package io.github.frenchntoasty.mixin.mixins;
 
-import java.util.Random;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.At;
-import net.minecraft.block.BlockLiquid;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(BlockLiquid.class)
-public abstract class MixinBlockLiquid {
- 
-    @Shadow public abstract void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand);
+@Mixin(WorldServer.class)
+public class MixinWorldServer {
+    @Redirect(method = "updateBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldServer;canBlockFreezeNoWater(Lnet/minecraft/util/math/BlockPos;)Z"))
+    private boolean redirectCanBlockFreezeNoWater(WorldServer instance, BlockPos blockPos) {
+        return false;
+    }
 
-    @Inject(method = "updateTick", at = @At("HEAD"), cancellable = true)
-    private void onUpdateTick(World worldIn, BlockPos pos, IBlockState state, Random rand, CallbackInfo ci) {
-        if (state.getBlock() == Blocks.WATER && !worldIn.canBlockSeeSky(pos)) {
-            // Check if the block is water and is not exposed to the sky
-            ci.cancel(); // Prevent the block from updating
-        } else {
-            updateTick(worldIn, pos, state, rand); // Call the original method to perform normal updates
-        }
+    @Redirect(method = "updateBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldServer;canSnowAt(Lnet/minecraft/util/math/BlockPos;Z)Z"))
+    private boolean redirectCanSnowAt(WorldServer instance, BlockPos blockPos, boolean checkLight) {
+        return false;
     }
 }
